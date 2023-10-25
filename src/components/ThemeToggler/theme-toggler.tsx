f31e2822d7130ede228884a4e56121415e9dd7bf
vasilkovalyov <vasilkovalyov@gmail.com>
2023-10-25 12:06:12 +0300

    refactored theme toggler

--- src/components/ThemeToggler/theme-toggler.tsx
+++ src/components/ThemeToggler/theme-toggler.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useState } from 'react';
 import styles from './theme-toggler.module.scss';
 import { useTheme } from 'next-themes';
 
@@ -6,7 +7,12 @@ import { FiMoon } from 'react-icons/fi';
 import { BsSun } from 'react-icons/bs';
 
 export default function ThemeToggler() {
+  const [mounted, setMounted] = useState<boolean>(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   function onToggleTheme() {
     if (theme === 'dark') {
@@ -14,6 +20,10 @@ export default function ThemeToggler() {
       return;
     }
     setTheme('dark');
+  }
+
+  if (!mounted) {
+    return null;
   }
 
   return (
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { Header } from './Header';
+export { Navbar } from './Navbar';
+export { Button } from './Button';
+export { ThemeToggler } from './ThemeToggler';
